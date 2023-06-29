6f438a0b6282ca08423b4f9f7d248ab3037b1637
Abe Omaits <aomaits@gmail.com>
2023-06-29 17:34:27 -0500

    initial loop created in javascript

--- script.js
+++ script.js
@@ -15,31 +15,33 @@ function writePassword() {
 generateBtn.addEventListener("click", writePassword);
 
 function generatePassword() {
-    
+    var selectedLength = window.prompt("Choose a password length between 8 and 128 characters");
+    var passwordLength = parseInt(selectedLength);
+        if (passwordLength <= 7) {
+            window.alert("That password is too short. Please try again.");
+            generatePassword(); }
+        else if (passwordLength >= 129) {
+            window.alert("That password is too long. Please try again.");
+        generatePassword(); }
+        else {
+            return;
+        }
+
 }
+    
+    var passwordCharacters = 0
+    
+    // selectedLength variable needs to be counted by the password generator
+    
 
-
-// var generateBtn = document.querySelector("#generate");
-
-// Write password to the #password input
-// function writePassword() {
-//   var password = generatePassword();
-//   var passwordText = document.querySelector("#password");
-
-//   passwordText.value = password;
-
-// }
-
-//  Add event listener to generate button
-// generateBtn.addEventListener("click", writePassword);
-
+// objective - series of prompts when button is pressed, user selects which criteria they want included(lowercase, uppercase, numeric, and/or special characters), generates password between 8 and 128 characters, If none are selected need to loop back and prompt again! Password should then show up on the page
 
 // function generatePassword() {
-//   prompt how many characters
+//   prompt how many characters (between 8 and 128)
 //   var for charcters
 
 //   What types of charctyers
-//   special, number, upper, lower
+//   make an array for each: special, number, upper, lower
 //   window.confirm
 
 //   Use answers to make password
