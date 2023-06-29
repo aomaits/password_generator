a530e7f841fe8457e01858645ca359c494968846
Abe Omaits <aomaits@gmail.com>
2023-06-29 16:38:50 -0500

    initial starter code and docs

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # password_generator
+
+# Credits
+All code in the HTML and CSS documents provided by the UT coding bootcamp. Starter code in JavaScript provided by same. 
---
+++ script.js
@@ -0,0 +1,55 @@
+
+// Assignment Code
+var generateBtn = document.querySelector("#generate");
+
+// Write password to the #password input
+function writePassword() {
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
+
+  passwordText.value = password;
+
+}
+
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
+
+function generatePassword() {
+    
+}
+
+
+// var generateBtn = document.querySelector("#generate");
+
+// Write password to the #password input
+// function writePassword() {
+//   var password = generatePassword();
+//   var passwordText = document.querySelector("#password");
+
+//   passwordText.value = password;
+
+// }
+
+//  Add event listener to generate button
+// generateBtn.addEventListener("click", writePassword);
+
+
+// function generatePassword() {
+//   prompt how many characters
+//   var for charcters
+
+//   What types of charctyers
+//   special, number, upper, lower
+//   window.confirm
+
+//   Use answers to make password
+//   vars that include all possible charcters
+//   seperate vars for each type, arrays
+//   example var upperLetters = ["A", "B", "C"]
+
+//   take all true arrays and concat into new array -loop
+//   choose random chars from arr based on user input
+//   var push into new array
+// var password = turn array into string .toString()
+
+// return password;
